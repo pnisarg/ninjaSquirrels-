57f25bffeec8d4d839ac46473fff9e7ddac2d7f7
pnisarg <pnisarg26@yahoo.com>
2016-02-05 18:37:34 -0800

    issues with output file format

--- problemSolver.py
+++ problemSolver.py
@@ -4,7 +4,7 @@ from node import Node
 class ProblemSolver:
     
     INFINITY = float("inf")
-    
+    logFile = "" 
     def __init__(self, initialState, boardValue, player, cuttingDepth):
         self.initialState = initialState
         self.boardValue = boardValue
@@ -61,34 +61,55 @@ class ProblemSolver:
         #if it is maximizing player
         if isMaxPlayer:
             bestValue = -self.INFINITY
+            self.logFile.write(node.name +","+ str(int(self.cuttingDepth) - int(node.depth))
+                               +","+ str(node.value).replace('inf','Infinity')+"\n")
             for child in node.children:
                 v = self.minimax(child, int(depth) - 1, False, problemObj)
                 child.value = v
                 bestValue = max(bestValue, v)
                 node.value = bestValue
-                print child.name+","+ str(int(self.cuttingDepth) - child.depth)+","+ str(child.value)
-                print node.name +","+ str(int(self.cuttingDepth) - int(node.depth))+","+ str(node.value)
-            print "Returning bestValue: ", bestValue
+                self.logFile.write(child.name+","+ str(int(self.cuttingDepth) - child.depth)
+                                   +","+ str(child.value).replace('inf','Infinity')+"\n")
+                self.logFile.write(node.name +","+ str(int(self.cuttingDepth) - int(node.depth))
+                                   +","+ str(node.value).replace('inf','Infinity')+"\n")
+                # print child.name+","+ str(int(self.cuttingDepth) - child.depth)+","+ str(child.value)
+                # print node.name +","+ str(int(self.cuttingDepth) - int(node.depth))+","+ str(node.value)
             return bestValue
         
         #if it is minimizing player
         else:
             bestValue = self.INFINITY
+            self.logFile.write(node.name +","+ str(int(self.cuttingDepth) - int(node.depth))
+                               +","+ str(node.value).replace('inf','Infinity')+"\n")
             for child in node.children:
                 v = self.minimax(child, int(depth) - 1, True, problemObj)
                 child.value = v
                 bestValue = min(bestValue, v)
                 node.value = bestValue
-                print child.name+","+ str(int(self.cuttingDepth) - child.depth)+","+ str(child.value)
-                print node.name +","+  str(int(self.cuttingDepth) - int(node.depth))+"," + str(node.value)
-            print "Returning bestValue: ", bestValue
+                self.logFile.write(child.name+","+ str(int(self.cuttingDepth) - child.depth)
+                                   +","+ str(child.value).replace('inf','Infinity')+"\n")
+                self.logFile.write(node.name +","+ str(int(self.cuttingDepth) - int(node.depth))
+                                   +","+ str(node.value).replace('inf','Infinity')+"\n")
+                # print child.name+","+ str(int(self.cuttingDepth) - child.depth)+","+ str(child.value)
+                # print node.name +","+  str(int(self.cuttingDepth) - int(node.depth))+"," + str(node.value)
             return bestValue
 
 
     def initMinMax(self):
         problemObj = Problem(self.initialState, self.boardValue,self.player)
+        self.logFile = open("traverse_log.txt", "w")
+        self.logFile.write("Node,Depth,Value" +"\n")
+        # self.logFile.write("root,0,-Infinity\n")
         node = Node(self.initialState, None, self.cuttingDepth,-float('inf'),"root",problemObj, self.player)   
-        print self.minimax(node, self.cuttingDepth, True, problemObj)
+        bestValue = self.minimax(node, self.cuttingDepth, True, problemObj)
+        for childNode in node.children:
+            if childNode.value == bestValue:
+                self.outputState(childNode.state)  #stores result in next_state.txt file
+                # self.printState(childNode.state)
+                # print childNode.value
+                break
+        self.logFile.close()
+
         
 
 
